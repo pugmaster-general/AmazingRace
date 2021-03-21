e5c55bbcf7923dab19b0bffa5040ecc28c18513a
pugmaster-general <66092816+pugmaster-general@users.noreply.github.com>
2021-03-20 22:05:57 -0400

    building the clues app

--- clues/models.py
+++ clues/models.py
@@ -1,3 +1,15 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
+import uuid
 
 # Create your models here.
+class Clue(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    clue_text = models.CharField(max_length=255, null=False)
+    isDetour = models.BooleanField(default=False, null=False)
+    showMap = models.BooleanField(default=False, null=False)
+    def __str__(self):
+        return self.clue_text
