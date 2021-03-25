7bab21d9f7ce88e247b6821fefc245e55e7370fd
ken <66092816+pugmaster-general@users.noreply.github.com>
2021-03-25 06:14:55 +0000

    adding pythonanywhere updates to repo

--- AmazingRace/settings.py
+++ AmazingRace/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '&&r8u(37y30-5wm5s2$)#kfuku(@&y*w-c-p0wo%k3nrj(*b32'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tigerboosterclub324th.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/tigerboosterclub324th/AmazingRace/static'
